06d265bbabeb45af3c7064f63184b5d3926f487d
hiro08gh <hiro08gh@gmail.com>
2020-08-23 16:33:30 +0900

    first commit:tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby microCMS Landing Page`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -16,19 +20,13 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-microcms",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        apiKey: process.env.API_KEY,
+        serviceId: process.env.SERVICE_ID,
+        format: 'object',
+        endpoint: "landing",
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql} from "gatsby";
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({data: {microcmsLanding} }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{microcmsLanding.element.heading}</h1>
+      <p>{microcmsLanding.element.paragraph1}</p>
+      <p>{microcmsLanding.element.paragraph2}</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+    </Layout>
+  )
+} 
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    microcmsLanding {
+      id
+      element {
+        heading
+        paragraph1
+        paragraph2
+      }
+    }
+  }
+`
